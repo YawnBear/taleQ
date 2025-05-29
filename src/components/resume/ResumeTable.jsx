e17693c631b4c467a9b4eeb449fa843069fced72
Your Name <you@example.com>
2025-05-29 16:08:28 +0800

    fix display column

--- src/components/resume/ResumeTable.jsx
+++ src/components/resume/ResumeTable.jsx
@@ -18,6 +18,7 @@ export default function ResumeTable({
     handleStatusChange,
     notifiedCandidates,
     setNotifiedCandidates,
+    handleColumnChange,
     onDeleteResume
 }) {
     const [selectedRows, setSelectedRows] = useState(new Set());
